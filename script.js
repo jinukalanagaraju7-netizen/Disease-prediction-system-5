f5c2fc3749690f09f518d52d3e7929c0f8c5e7ce
jinukalanagaraju7-netizen <jinukalanagaraju7@gmail.com>
2026-03-13 18:39:40 +0530

    Refactor login and openDoors functions
    Refactor openDoors function to execute on window load and add delay for opening doors.

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-function openDoors(){
+window.onload=function(){
+
+setTimeout(function(){
+
+document.getElementById("doors").classList.add("open");
+
+},800);
+
+}
+
+function login(){
 
 var user=document.getElementById("username").value;
 var pass=document.getElementById("password").value;
 
 if(user=="admin" && pass=="1234"){
 
-document.querySelector(".door-container").classList.add("open");
-
-setTimeout(function(){
 window.location="dashboard.html";
-},2000);
 
 }
-
 else{
-alert("Wrong Username or Password");
+
+document.getElementById("error").innerHTML="Invalid Login";
+
 }
 
 }
