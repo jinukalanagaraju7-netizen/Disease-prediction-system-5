e7887dce80b16ab2f826b8b8c5fe20de1b647b54
jinukalanagaraju7-netizen <jinukalanagaraju7@gmail.com>
2026-03-17 20:02:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ let medicine="";
 
 if(count<2){
 
-disease="Please select minimum 2 symptoms";
-medicine="";
+disease="Select at least 2 symptoms";
 
 }
 
@@ -20,38 +19,39 @@ let cough=document.querySelector("input[value='Cough']").checked;
 let headache=document.querySelector("input[value='Headache']").checked;
 let cold=document.querySelector("input[value='Cold']").checked;
 let chest=document.querySelector("input[value='Chest Pain']").checked;
+let rash=document.querySelector("input[value='Skin Rash']").checked;
 
 if(fever && cough){
 
 disease="Common Cold";
-medicine="Paracetamol, Cetirizine, Drink warm water";
+medicine="Paracetamol, Cetirizine";
 
 }
 
 else if(fever && headache){
 
 disease="Viral Fever";
-medicine="Paracetamol, Rest, Drink fluids";
+medicine="Paracetamol, Rest";
 
 }
 
 else if(cough && chest){
 
 disease="Bronchitis";
-medicine="Cough syrup, Steam inhalation";
+medicine="Cough Syrup";
 
 }
 
-else if(cold && cough){
+else if(rash){
 
-disease="Flu";
-medicine="Antiviral medicine, Rest";
+disease="Skin Allergy";
+medicine="Antihistamine";
 
 }
 
 else{
 
-disease="Possible infection";
+disease="Possible Infection";
 medicine="Consult doctor";
 
 }
